9609742528d5de9c538d6358eb3bd69f2317e598
Arindam Rawat <rawatarindam95@gmail.com>
2025-08-12 20:00:02 +0530

    Modification for vercel

--- app.py
+++ app.py
@@ -139,6 +139,6 @@ def rebalance():
         return jsonify({"error": str(e)})
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=False)
